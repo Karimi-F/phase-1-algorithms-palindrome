1e3c00b4519811fc03efcbba0bfc8058c8777242
Karimi-F <fridah37karimi@gmail.com>
2024-10-22 14:00:11 +0300

    Checks if string is a palindrome

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function isPalindrome(word) {
+function isPalindrome(str) {
   // Write your algorithm here
+  const newString = str.toLowerCase();
+
+  const reversedString = newString.split('').reverse().join('');
+
+  return newString === reversedString;
 }
 
 /* 
@@ -15,11 +20,18 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+  console.log("=>", isPalindrome("abba"));
+  console.log("=>", isPalindrome("a"));
+  console.log("=>", isPalindrome("deed"));
+  console.log("=>", isPalindrome("rotator"));
 
   console.log("");
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  console.log("=>", isPalindrome("ab"));
+  console.log("=>", isPalindrome("peephole"));
+  console.log("=>", isPalindrome("test"));
 }
 
 module.exports = isPalindrome;
